717e5a4e38152a1497c5bfbc71e3424729eaa948
Student <example@example.com>
2019-12-10 13:25:48 -0800

    Stack form inputs

--- src/components/SettingsForm.jsx
+++ src/components/SettingsForm.jsx
@@ -7,9 +7,9 @@ function SettingsForm(props) {
     <div>
       <form onSubmit= {(event) => props.onSubmit(event, _bombCount, _dimension)}>
         <label for='bomb'>How many bombs?</label>
-        <input id='bomb' type='number' name='bombs' ref={input => _bombCount = input}/>
+        <input id='bomb' type='number' name='bombs' ref={input => _bombCount = input}/><br/>
         <label for='dimension'>What size grid would you like? </label>
-        <input id='dimension' type='number' ref={input => _dimension = input}/>
+        <input id='dimension' type='number' ref={input => _dimension = input}/><br/>
         <button type='submit'>Start</button>
       </form>
     </div>
